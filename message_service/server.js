d4931e254dfd26303244fed28e19c77c49f1484c
KeithLoh <keithloh99@gmail.com>
2023-03-10 18:19:10 +0800

    Added logger for better debugging,code clean up, Added mongoDB and tested connection

--- message_service/server.js
+++ message_service/server.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const logger = require("./utils/logger");
 const appRoute = require("./routes/route.js");
+const { port } = require("./env.js");
+const connect = require("./utils/connect.js");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || port;
+logger.info(port);
 
 app.use(express.json());
 
 // Routes
 app.use("/api", appRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT} !`);
+app.listen(PORT, async () => {
+  logger.info(`Server is running on http://localhost:${PORT}!`);
+
+  await connect();
 });
