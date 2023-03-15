f3c259cb903e31d798e22a9f0ebe733aa1464a1b
KeithLoh <keithloh99@gmail.com>
2023-03-15 16:34:02 +0800

    RabbitMQ AMQP from post request to send email object via queue

---
+++ create_booking_complex_service/README.md
@@ -0,0 +1,8 @@
+# create_booking_complex_service
+
+### To Run RabbitMQ Docker
+` docker start rabbitmq-mgmt `
+
+### To Stop Rabbit MQ Docker
+` docker stop rabbitmq-mgmt `
+
--- create_booking_complex_service/get_booking.py
+++ create_booking_complex_service/get_booking.py
@@ -3,19 +3,76 @@ import os
 from flask import Flask, render_template, request, url_for, redirect,jsonify
 from flask_cors import CORS
 from os import environ
-from pymongo import MongoClient
-from bson import json_util
-from bson.objectid import ObjectId
+# from pymongo import MongoClient
+# from bson import json_util
+# from bson.objectid import ObjectId
 
 from datetime import datetime
 import json
+import pika
+
+import amqp_setup
 
 app = Flask(__name__)
 
 CORS(app)  
 
-client = MongoClient(host='localhost',
-                        port=27017
-                        )
-
+# Mongo Client
+# client = MongoClient(host='localhost',
+#                         port=27017
+#                         )
 # 
+
+
+
+
+"""
+Order JSON Object parsed into ProcessEmailService should follow this format
+{
+userEmail: String,
+userName: String,
+orderID: String,
+courseName: String,
+coursePrice: String,
+courseDescription: String
+}
+
+{
+    "userEmail" : "celov54484@gpipes.com",
+    "userName" : "celo",
+    "orderID" : "4500",
+    "courseName" : "Data Structure Algorithms",
+    "coursePrice" : "$2000",
+    "courseDescription" : "A 3rd semester course at SMU, continues to develop students' understanding of object oriented programming, memory management"
+}
+
+"""
+
+
+# Route to test if this complex microservice, using AMQP can call message_service
+@app.route("/emailservice", methods=['POST'])
+def processEmailService():
+
+    # order = request.json.get()
+    order = request.get_json()
+    # print(order)
+    order_msg = json.dumps(order)
+    print("order msg is after this")
+    print(order_msg)
+    # Send the order object to the messaging microservice
+    amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="message.", 
+            body=order_msg, properties=pika.BasicProperties(delivery_mode = 2))
+    
+    # Test Return statement
+    return {
+        "code": 201,
+        "data": {
+            "order": "success"
+        }
+    }
+
+
+if __name__ == "__main__":
+    print("This is flask " + os.path.basename(__file__) + " to register for a class")
+    app.run(host="0.0.0.0", port=5007, debug=True)
+print(f"Flask app is initialized on port 5007")
